c9ee65152b5b036426f5d17a9436f0a4e2fd6310
Ivan Roa <naviroa92@Ivans-MacBook-Pro.local>
2024-07-03 15:19:50 -0500

    test 7 password

--- json-services-cards-bienestar.py
+++ json-services-cards-bienestar.py
@@ -2,11 +2,17 @@ import json
 import gspread
 from google.oauth2.service_account import Credentials
 
+# Define los ámbitos necesarios para acceder a Google Sheets y Google Drive
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.readonly"
+]
+
 # Lee las credenciales desde el archivo client_secrets.json
 with open('client_secrets.json') as f:
     creds_dict = json.load(f)  # Asegúrate de que el archivo tiene un JSON válido
 
-creds = Credentials.from_service_account_info(creds_dict)
+creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
 gc = gspread.authorize(creds)
 
 # Abre tu hoja de cálculo por su ID
